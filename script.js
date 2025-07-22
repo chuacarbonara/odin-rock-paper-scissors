fa3546999730ce3d5af8bc54a3213e43b996641f
Elisha Chua <218030632+chuacarbonara@users.noreply.github.com>
2025-07-23 00:15:11 +0800

    Add contents to script.js

--- script.js
+++ script.js
@@ -1,1 +1,78 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+  let CCNum = Math.floor(Math.random() * 3) + 1;
+  if (CCNum == 1) {
+    return "rock";
+  }
+  else if (CCNum == 2) {
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+}
+
+
+function getHumanChoice() {
+  let HCNum = prompt("Rock, paper or scissors?");
+
+  if (HCNum.toLowerCase() === "rock") {
+    return "rock";
+  } else if (HCNum.toLowerCase() === "paper") {
+    return "paper";
+  } else if (HCNum.toLowerCase() === "scissors") {
+    return "scissors";
+  }
+};
+
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    console.log("It's a tie!");
+    return;
+  }
+  if (humanChoice == "rock") {
+    switch (computerChoice) {
+        case "paper":
+            console.log("Defeat!");
+            computerScore ++;
+            break;
+        default:
+            console.log("Victory!");
+            humanScore ++;
+    }
+  } else if (humanChoice == "paper") {
+    switch (computerChoice) {
+        case "rock":
+            console.log("Victory!");
+            humanScore ++;
+            break;
+        default:
+            console.log("Defeat!");
+            computerScore ++;
+    }
+  } else if (humanChoice == "scissors") {
+    switch (computerChoice) {
+        case "rock":
+            console.log("Defeat!");
+            computerScore ++;
+            break;
+        default:
+            console.log("Victory!");
+            humanScore ++;
+    }
+  }
+}
+
+function playGame(){
+    for (let i=0; i<5; i++){
+        let computerChoice=getComputerChoice();
+        let humanChoice = getHumanChoice();
+        playRound(humanChoice,computerChoice);
+        console.log('Computer:',computerChoice,'You:',humanChoice,'\nComputer score:',computerScore,'Your score:',humanScore);
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+playGame();
